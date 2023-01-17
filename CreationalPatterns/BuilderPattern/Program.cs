c800c52a835f6d97d68c700c8be93d2a5acf8b87
Reza Mashayekhi <89461977+mashayekhi-reza@users.noreply.github.com>
2023-01-17 16:29:54 +0100

    builder pattern structural implementation added

---
+++ CreationalPatterns/BuilderPattern/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BuilderPattern.Structural;
+
+Console.WriteLine("Hello, World!");
+
+StructuralImplementation();
+
+Console.ReadKey();
+
+static void StructuralImplementation()
+{
+	Director director = new();
+
+	ConcreteBuilderA concreteBuilderA = new();
+	ConcreteBuilderB concreteBuilderB = new();
+
+	director.Contruct(concreteBuilderA);
+	Product productA = concreteBuilderA.GetProduct();
+
+	director.Contruct(concreteBuilderB);
+	Product productB = concreteBuilderB.GetProduct();
+
+	Console.WriteLine("Product A Parts:");
+	PrintProductParts(productA);
+
+	Console.WriteLine("Product B Parts:");
+	PrintProductParts(productB);
+}
+
+static void PrintProductParts(Product product)
+{
+	product.GetParts().ForEach(p => Console.WriteLine(p));
+}
