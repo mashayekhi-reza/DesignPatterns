062528858477b4c48bdb101a9e340bccb3c041c4
Reza Mashayekhi <89461977+mashayekhi-reza@users.noreply.github.com>
2023-01-18 09:46:50 +0100

    fluent builder pattern an example implementation added

--- CreationalPatterns/BuilderPatterns/FluentBuilderPattern/Program.cs
+++ CreationalPatterns/BuilderPatterns/FluentBuilderPattern/Program.cs
@@ -1,28 +1,74 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using FluentBuilderPattern.Example;
 using FluentBuilderPattern.Structural;
 
 Console.WriteLine("Fluent Builder Pattern");
 
-ConcreteBuilderA concreteBuilderA = new();
-ConcreteBuilderB concreteBuilderB = new();
+StructuralImplementation();
 
-Product productA = concreteBuilderA
-	.CreatePartA()
-	.CreatePartB()
-	.Build();
+ExampleImplementation();
 
-Product productB = concreteBuilderB
-	.CreatePartA()
-	.CreatePartB()
-	.Build();
+Console.ReadKey();
 
-Console.WriteLine("Product A Parts:");
-PrintProductParts(productA);
+static void StructuralImplementation()
+{
+	ConcreteBuilderA concreteBuilderA = new();
+	ConcreteBuilderB concreteBuilderB = new();
 
-Console.WriteLine("Product B Parts:");
-PrintProductParts(productB);
+	Product productA = concreteBuilderA
+		.CreatePartA()
+		.CreatePartB()
+		.Build();
+
+	Product productB = concreteBuilderB
+		.CreatePartA()
+		.CreatePartB()
+		.Build();
+
+	Console.WriteLine("Product A Parts:");
+	PrintProductParts(productA);
+
+	Console.WriteLine("Product B Parts:");
+	PrintProductParts(productB);
+}
 
 static void PrintProductParts(Product product)
 {
 	product.GetParts().ForEach(p => Console.WriteLine(p));
+}
+
+static void ExampleImplementation()
+{
+	BenzBuilder benzBuilder = new();
+	BmwBuilder bmwBuilder = new();
+
+	Vehicle benz = benzBuilder
+		.SetType()
+		.SetBrand()
+		.AddEngine()
+		.AddBody()
+		.Build();
+
+	Vehicle bmw = bmwBuilder
+		.SetType()
+		.SetBrand()
+		.AddEngine()
+		.AddBody()
+		.Build();
+
+	Console.WriteLine("Benz Specifications:");
+	PrintVehicleParts(benz);
+
+	Console.WriteLine("Bmw Specifications:");
+	PrintVehicleParts(bmw);
+}
+
+static void PrintVehicleParts(Vehicle vehicle)
+{
+	Console.WriteLine(vehicle.Type);
+	Console.WriteLine(vehicle.Brand);
+	foreach (var kvp in vehicle.GetParts())
+	{
+		Console.WriteLine("{0} = {1}", kvp.Key, kvp.Value);
+	}
 }
