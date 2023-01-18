40d512c5ba3066e4c03b7035dd2b86b86db0cfa7
Reza Mashayekhi <89461977+mashayekhi-reza@users.noreply.github.com>
2023-01-18 09:33:53 +0100

    fluent builder pattern structural implementation added

---
+++ CreationalPatterns/BuilderPatterns/FluentBuilderPattern/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FluentBuilderPattern.Structural;
+
+Console.WriteLine("Fluent Builder Pattern");
+
+ConcreteBuilderA concreteBuilderA = new();
+ConcreteBuilderB concreteBuilderB = new();
+
+Product productA = concreteBuilderA
+	.CreatePartA()
+	.CreatePartB()
+	.Build();
+
+Product productB = concreteBuilderB
+	.CreatePartA()
+	.CreatePartB()
+	.Build();
+
+Console.WriteLine("Product A Parts:");
+PrintProductParts(productA);
+
+Console.WriteLine("Product B Parts:");
+PrintProductParts(productB);
+
+static void PrintProductParts(Product product)
+{
+	product.GetParts().ForEach(p => Console.WriteLine(p));
+}
