5eb8bdade371d4ffe6bd0cc8f37f0af49539b6ec
Reza Mashayekhi <89461977+mashayekhi-reza@users.noreply.github.com>
2023-01-18 14:50:57 +0100

    sequential fluent builder structural and example added

---
+++ CreationalPatterns/BuilderPatterns/SequentialFluentBuilder/Program.cs
@@ -0,0 +1,75 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SequentialFluentBuilder.Example;
+using SequentialFluentBuilder.Structural;
+
+Console.WriteLine("Sequential Fluent Interface");
+
+StructuralImplementation();
+
+ExampleImplementation();
+
+Console.ReadKey();
+
+static void StructuralImplementation()
+{
+	IBuilder concreteBuilderA = new ConcreteBuilderA();
+	IBuilder concreteBuilderB = new ConcreteBuilderB();
+	
+	Product productA = concreteBuilderA
+		.CreatePartA()
+		.CreatePartB()
+		.Build();
+
+	Product productB = concreteBuilderB
+		.CreatePartA()
+		.CreatePartB()
+		.Build();
+
+
+	Console.WriteLine("Product A Parts:");
+	PrintProductParts(productA);
+
+	Console.WriteLine("Product B Parts:");
+	PrintProductParts(productB);
+}
+
+static void PrintProductParts(Product product)
+{
+	product.GetParts().ForEach(p => Console.WriteLine(p));
+}
+
+static void ExampleImplementation()
+{
+	IVehicleBuilder benzBuilder = new BenzBuilder();
+	IVehicleBuilder bmwBuilder = new BmwBuilder();
+
+	Vehicle benz = benzBuilder
+		.SetType()
+		.SetBrand()
+		.AddEngine()
+		.AddBody()
+		.Build();
+
+	Vehicle bmw = bmwBuilder
+		.SetType()
+		.SetBrand()
+		.AddEngine()
+		.AddBody()
+		.Build();
+
+	Console.WriteLine("Benz Specifications:");
+	PrintVehicleParts(benz);
+
+	Console.WriteLine("Bmw Specifications:");
+	PrintVehicleParts(bmw);
+}
+
+static void PrintVehicleParts(Vehicle vehicle)
+{
+	Console.WriteLine(vehicle.Type);
+	Console.WriteLine(vehicle.Brand);
+	foreach (var kvp in vehicle.GetParts())
+	{
+		Console.WriteLine("{0} = {1}", kvp.Key, kvp.Value);
+	}
+}
---
+++ CreationalPatterns/BuilderPatterns/SequentialFluentBuilder/Structural/ConcreteBuilderA.cs
@@ -0,0 +1,26 @@
+﻿namespace SequentialFluentBuilder.Structural
+{
+	public class ConcreteBuilderA : IBuilder, IWithPartB, IBuild
+	{
+		private readonly Product _product = new();
+
+		public IWithPartB CreatePartA()
+		{
+			_product.AddPart("PartA");
+			return this;
+		}
+
+		public IBuild CreatePartB()
+		{
+			_product.AddPart("PartB");
+			return this;
+		}
+
+		public Product Build()
+		{
+			return _product;
+		}
+	}
+}
+
+
