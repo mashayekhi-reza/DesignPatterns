5d259ca4275547fa123cb3b86b266fe209a261d6
Reza Mashayekhi <89461977+mashayekhi-reza@users.noreply.github.com>
2023-01-25 17:21:42 +0100

    singleton pattern added

---
+++ CreationalPatterns/SingletonPattern/2_SingletonWithLazyInitialization.cs
@@ -0,0 +1,23 @@
+﻿namespace SingletonPattern
+{
+	public class SingletonWithLazyInitialization
+	{
+		private static SingletonWithLazyInitialization? _instance;
+		
+		protected SingletonWithLazyInitialization()
+		{
+		}
+
+		public static SingletonWithLazyInitialization Instance
+		{
+			get
+			{
+				if (_instance == null)
+				{
+					_instance = new SingletonWithLazyInitialization();
+				}
+				return _instance;
+			}
+		}
+	}
+}
---
+++ CreationalPatterns/SingletonPattern/3_SingletonThreadSafeWithLazyInitialization.cs
@@ -0,0 +1,14 @@
+﻿namespace SingletonPattern
+{
+	public class SingletonThreadSafeWithLazyInitialization
+	{
+		private static readonly Lazy<SingletonThreadSafeWithLazyInitialization> _instance
+			= new(() => new SingletonThreadSafeWithLazyInitialization());
+
+		protected SingletonThreadSafeWithLazyInitialization()
+		{
+		}
+
+		public static SingletonThreadSafeWithLazyInitialization Instance => _instance.Value;
+	}
+}
