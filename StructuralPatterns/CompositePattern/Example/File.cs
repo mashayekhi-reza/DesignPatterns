05aa4bce39a8d8c01744989ecb1bc44db0a4e68e
Reza Mashayekhi <89461977+mashayekhi-reza@users.noreply.github.com>
2023-02-01 12:30:17 +0100

    composite pattern and example implementation added

---
+++ StructuralPatterns/CompositePattern/Example/File.cs
@@ -0,0 +1,18 @@
+﻿namespace CompositePattern.Example
+{
+	public class File : ISystemItem
+	{
+		private readonly string _name;
+		private readonly double _size;
+
+		public File(string name, double size)
+		{
+			_name = name;
+			_size = size;
+		}
+
+		public string Name => _name;
+
+		public double GetSize() => _size;
+	}
+}
--- StructuralPatterns/CompositePattern/Program.cs
+++ StructuralPatterns/CompositePattern/Program.cs
@@ -1,9 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using CompositePattern.Example;
 using CompositePattern.Structural;
+using File = CompositePattern.Example.File;
 
 Console.WriteLine("Composite Pattern");
 
 StructuralImplementation();
+
+ExampleImplementation();
 
 Console.ReadKey();
 
@@ -26,5 +30,38 @@ static void StructuralImplementation()
 	root.Add(compositeA);
 
 	Console.WriteLine(root.Operation());
+}
 
+static void ExampleImplementation()
+{
+	Console.WriteLine("\nComposite Pattern Example Implementation\n");
+
+	var firstFolder = new Folder("firstFolder");
+	firstFolder.AddItem(new File("file1", 1));
+	firstFolder.AddItem(new File("file2", 2));
+
+	var secondFolder = new Folder("secondFolder");
+	secondFolder.AddItem(new File("file3", 3));
+	secondFolder.AddItem(new File("file4", 4));
+
+
+	var thirdFolder = new Folder("thirdFolder");
+
+	secondFolder.AddItem(thirdFolder);
+	firstFolder.AddItem(secondFolder);
+
+	PrintFoldersSize(firstFolder, secondFolder, thirdFolder);
+
+	Console.WriteLine("\nAdd one file to second folder and one to third folder\n");
+	secondFolder.AddItem(new File("file5", 5));
+	thirdFolder.AddItem(new File("file6", 6));
+
+	PrintFoldersSize(firstFolder, secondFolder, thirdFolder);
+}
+
+static void PrintFoldersSize(Folder firstFolder, Folder secondFolder, Folder thirdFolder)
+{
+	Console.WriteLine($"{nameof(firstFolder)}, Size: {firstFolder.GetSize()}");
+	Console.WriteLine($"{nameof(secondFolder)}, Size: {secondFolder.GetSize()}");
+	Console.WriteLine($"{nameof(thirdFolder)}, Size: {thirdFolder.GetSize()}");
 }
