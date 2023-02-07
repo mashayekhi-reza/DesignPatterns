8ff8e660f363bf582716ac9063788623db28d65d
Reza Mashayekhi <89461977+mashayekhi-reza@users.noreply.github.com>
2023-02-07 17:02:41 +0100

    flyweight pattern structural implementation added

---
+++ StructuralPatterns/FlyweightPattern/Program.cs
@@ -0,0 +1,32 @@
+﻿using FlyweightPattern.Structural;
+
+Console.WriteLine("Flyweight Pattern");
+
+StructuralImplementation();
+
+Console.ReadKey();
+
+static void StructuralImplementation()
+{
+	Console.WriteLine("\nFlyweight Pattern Structural Implementation\n");
+
+	var factory = new FlyweightFactory();
+
+	Console.WriteLine("The key is A and the extrinsic state is 1:");
+	factory.GetFlyweight("A").Operation("1");
+
+	Console.WriteLine("The key is B and the extrinsic state is 2:");
+	factory.GetFlyweight("B").Operation("2");
+
+	Console.WriteLine("The key is C and the extrinsic state is 3:");
+	factory.GetFlyweight("C").Operation("3");
+
+	Console.WriteLine("The key is A and the extrinsic state is 4:");
+	factory.GetFlyweight("A").Operation("4");
+
+	Console.WriteLine("Unshare Flyweight and the extrinsic state is 5:");
+	factory.GetUnsharedFlyweight().Operation("5");
+
+	Console.WriteLine("Unshare Flyweight and the extrinsic state is 6:");
+	factory.GetUnsharedFlyweight().Operation("6");
+}
---
+++ StructuralPatterns/FlyweightPattern/Structural/UnsharedConcreteFlyweight.cs
@@ -0,0 +1,16 @@
+﻿namespace FlyweightPattern.Structural;
+
+public class UnsharedConcreteFlyweight : IFlyweight
+{
+	public int IntrinsicState { get; }
+
+	public UnsharedConcreteFlyweight()
+	{
+		IntrinsicState = Random.Shared.Next(1, 100);
+	}
+
+	public void Operation(string extrinsicstate)
+	{
+		Console.WriteLine($"From Unshared: Intrinsic State is {IntrinsicState}, and extrinsic state is {extrinsicstate}");
+	}
+}
