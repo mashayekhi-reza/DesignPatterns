aeb019e8e8051f7214eaf49304f9b8da7875b579
Reza Mashayekhi <89461977+mashayekhi-reza@users.noreply.github.com>
2023-02-09 08:02:53 +0100

    strategy pattern structural implementation added

---
+++ BehavioralPatterns/StrategyPattern/Program.cs
@@ -0,0 +1,21 @@
+﻿using StrategyPattern.Structural;
+
+Console.WriteLine("Strategy Pattern");
+
+StructuralImplementation();
+
+Console.ReadKey();
+
+static void StructuralImplementation()
+{
+	Console.WriteLine("\nStrategy Pattern Structural Implementation\n");
+
+	var concreteStrategyA = new ConcreteStrategyA();
+	var contextA = new Context(concreteStrategyA);
+	contextA.ContextInterface();
+
+	var concreteStrategyB = new ConcreteStrategyB();
+	var contextB = new Context(concreteStrategyB);
+	contextB.ContextInterface();
+}
+
