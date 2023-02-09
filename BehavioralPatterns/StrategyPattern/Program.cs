c40603fe3a421a4ee2b459f26cf7f21e2ff709ce
Reza Mashayekhi <89461977+mashayekhi-reza@users.noreply.github.com>
2023-02-09 10:45:42 +0100

    strategy pattern an example implementation added

--- BehavioralPatterns/StrategyPattern/Program.cs
+++ BehavioralPatterns/StrategyPattern/Program.cs
@@ -1,8 +1,11 @@
-﻿using StrategyPattern.Structural;
+﻿using StrategyPattern.Example;
+using StrategyPattern.Structural;
 
 Console.WriteLine("Strategy Pattern");
 
 StructuralImplementation();
+
+ExampleImplementation();
 
 Console.ReadKey();
 
@@ -17,5 +20,16 @@ static void StructuralImplementation()
 	var concreteStrategyB = new ConcreteStrategyB();
 	var contextB = new Context(concreteStrategyB);
 	contextB.ContextInterface();
+}
+
+static void ExampleImplementation()
+{
+	Console.WriteLine("\nStrategy Pattern Example Implementation\n");
+
+	var document = new Document("The document title", "This is the document body!");
+
+	document.Export(new CsvExportService());
+
+	document.Export(new JsonExportService());
 }
 
