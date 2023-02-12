a445bf4305be9a79fbcf7e681fbeb3b62cc97262
Reza Mashayekhi <89461977+mashayekhi-reza@users.noreply.github.com>
2023-02-12 14:14:10 +0100

    command pattern structural implementation added

---
+++ BehavioralPatterns/CommandPattern/Program.cs
@@ -0,0 +1,20 @@
+﻿using CommandPattern.Structural;
+
+Console.WriteLine("Command Pattern");
+
+StructuralImplementation();
+
+Console.ReadKey();
+
+static void StructuralImplementation()
+{
+	Console.WriteLine("\nCommand Pattern Structural Implementation\n");
+
+	var receiver = new Receiver();
+	ICommand command = new ConcreteCommand(receiver);
+	var invoker = new Invoker();
+
+	invoker.SetCommand(command);
+	invoker.ExecuteCommand();
+	invoker.UndoCommand();
+}
---
+++ BehavioralPatterns/CommandPattern/Structural/Receiver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CommandPattern.Structural;
+
+public class Receiver
+{
+	public void Action()
+	{
+		Console.WriteLine($"{GetType().Name}.{System.Reflection.MethodBase.GetCurrentMethod()?.Name}");
+	}
+
+	internal void UndoAction()
+	{
+		Console.WriteLine($"{GetType().Name}.{System.Reflection.MethodBase.GetCurrentMethod()?.Name}");
+	}
+}
