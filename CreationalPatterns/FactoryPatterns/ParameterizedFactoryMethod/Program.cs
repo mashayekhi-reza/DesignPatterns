72a8ac2188b72b9a5881f2be077379eb1322b21d
Reza Mashayekhi <89461977+mashayekhi-reza@users.noreply.github.com>
2023-02-14 09:30:04 +0100

    refactor structural implementation

--- CreationalPatterns/FactoryPatterns/ParameterizedFactoryMethod/Program.cs
+++ CreationalPatterns/FactoryPatterns/ParameterizedFactoryMethod/Program.cs
@@ -8,6 +8,8 @@ StructuralImplementation();
 
 ExampleImplementation();
 
+StructuralWithoutSwitchCaseImplementation();
+
 Console.ReadKey();
 
 static void StructuralImplementation()
@@ -30,4 +32,15 @@ static void ExampleImplementation()
 	Console.WriteLine($"Order Car to Drive: {factory.CreateVehicle("Car").Drive()}");
 	Console.WriteLine($"Order Boat to Drive: {factory.CreateVehicle("Boat").Drive()}");
 	Console.WriteLine($"Order Bicycle to Drive: {factory.CreateVehicle("Bicycle").Drive()}");
+}
+
+static void StructuralWithoutSwitchCaseImplementation()
+{
+	Console.WriteLine("\nStructural Implementation Without Switch Case\n");
+
+	IFactory factory = new ConcreteFactoryWithoutSwitchCase();
+
+	Console.WriteLine($"Key is A, the product is: {factory.CreateProduct("A").GetType().Name}");
+	Console.WriteLine($"Key is B, the product is: {factory.CreateProduct("B").GetType().Name}");
+	Console.WriteLine($"Key is C, which is not available, the product is: {factory.CreateProduct("C").GetType().Name}");
 }
---
+++ CreationalPatterns/FactoryPatterns/ParameterizedFactoryMethod/Structural/Factory/ConcreteFactoryWithoutSwitchCase.cs
@@ -0,0 +1,18 @@
+﻿using ParameterizedFactoryMethod.Structural.Products;
+
+namespace ParameterizedFactoryMethod.Structural.Factory
+{
+	public class ConcreteFactoryWithoutSwitchCase : IFactory
+	{
+		private readonly Dictionary<string, Func<IProduct>> _concereteImplementations = new () 
+		{
+			{ "A", () => new ConcreteProductA() },
+			{ "B", () => new ConcreteProductB() }
+		};
+		public IProduct CreateProduct(string key)
+		{
+			return _concereteImplementations.TryGetValue(key, out Func<IProduct>? implementation) ?
+				implementation() : new ConcreteProductDefault();
+		}
+	}
+}
