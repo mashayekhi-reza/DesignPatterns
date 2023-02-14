280babac8d2caf73c76d47e255462796a5d4a302
Reza Mashayekhi <89461977+mashayekhi-reza@users.noreply.github.com>
2023-02-14 10:03:53 +0100

    chain of responsibility pattern an example implementation added

--- BehavioralPatterns/ChainOfResponsibilityPattern/Program.cs
+++ BehavioralPatterns/ChainOfResponsibilityPattern/Program.cs
@@ -1,8 +1,11 @@
-﻿using ChainOfResponsibilityPattern.Structural;
+﻿using ChainOfResponsibilityPattern.Example;
+using ChainOfResponsibilityPattern.Structural;
 
 Console.WriteLine("Chain of Responsibility Pattern");
 
 StructuralImplementation();
+
+ExampleImplementation();
 
 Console.ReadKey();
 
@@ -21,4 +24,35 @@ static void StructuralImplementation()
     {
         handlerA.Handle(request);
     }
+}
+
+static void ExampleImplementation()
+{
+	Console.WriteLine("\nChain of Responsibility Pattern Example Implementation\n");
+
+	var titleHander = new DocumentTitleHandler();
+	var bodyHandler = new DocumentBodyHandler();
+	titleHander.SetSuccessor(bodyHandler);
+
+	var documentA = new Document("", "body");
+	var documentB = new Document("title", "");
+	var documentC = new Document("title", "body");
+
+	ValidateDocument(titleHander, documentA);
+	ValidateDocument(titleHander, documentB);
+	ValidateDocument(titleHander, documentC);
+}
+
+static void ValidateDocument(IHandler<Document> titleHander, Document document)
+{
+	Console.WriteLine($"Title: {document.Title}, Body: {document.Body}");
+	try
+	{
+		titleHander.Handle(document);
+		Console.WriteLine("The document is valid\n");
+	}
+	catch (Exception ex)
+	{
+		Console.WriteLine($"{ex.Message}\n");
+	}
 }
