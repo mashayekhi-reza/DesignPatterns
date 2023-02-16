7765b19a969109b488b7b3146a9ebcb247207cd2
Reza Mashayekhi <89461977+mashayekhi-reza@users.noreply.github.com>
2023-02-16 08:04:37 +0100

    state pattern structural implementation added

---
+++ BehavioralPatterns/StatePattern/Program.cs
@@ -0,0 +1,21 @@
+﻿using StatePattern.Structural;
+
+Console.WriteLine("State Pattern");
+
+StructuralImplementation();
+
+Console.ReadKey();
+
+static void StructuralImplementation()
+{
+	Console.WriteLine("\nState Pattern Structural Implementation\n");
+
+	var context = new Context(new ConcreteStateA());
+
+	for (var i = 0; i < 4; i++)
+	{
+		Console.WriteLine($"Current State: {context.State.GetType().Name}");
+
+		context.Request();
+	}
+}
