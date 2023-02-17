512eb4e881d10288e3600cdeb753608e33029cfb
Reza Mashayekhi <89461977+mashayekhi-reza@users.noreply.github.com>
2023-02-17 17:34:26 +0100

    iterator pattern structural implementation added

---
+++ BehavioralPatterns/IteratorPattern/Program.cs
@@ -0,0 +1,27 @@
+﻿using IteratorPattern.Structural;
+
+Console.WriteLine("Iterator Pattern");
+
+StructuralImplementation();
+
+Console.ReadKey();
+
+static void StructuralImplementation()
+{
+	Console.WriteLine("\nIterator Pattern Structural Implementation\n");
+
+	var concreteAggregate = new ConcreteAggregate();
+	concreteAggregate.Add("Item A");
+	concreteAggregate.Add("Item B");
+	concreteAggregate.Add("Item C");
+	concreteAggregate.Add("Item D");
+
+	IIterator iterator = concreteAggregate.CreateIterator();
+
+	Console.WriteLine("Start Iterating");
+
+	for(var obj = iterator.First(); !iterator.IsDone(); obj = iterator.Next())
+	{
+		Console.WriteLine(obj);
+	}
+}
