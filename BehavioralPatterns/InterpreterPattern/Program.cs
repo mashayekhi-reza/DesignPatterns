2f84d3b46a39252123a6a4c73414e0882eeeecba
Reza Mashayekhi <89461977+mashayekhi-reza@users.noreply.github.com>
2023-02-19 13:02:54 +0100

    interpreter pattern structural implementation added

---
+++ BehavioralPatterns/InterpreterPattern/Program.cs
@@ -0,0 +1,25 @@
+﻿using InterpreterPattern.Structural;
+
+Console.WriteLine("Interpreter Pattern");
+
+StructuralImplementation();
+
+Console.ReadKey();
+
+static void StructuralImplementation()
+{
+	Console.WriteLine("\nInterpreter Pattern Structural Implementation\n");
+
+	var context = new Context();
+
+	var expressions = new List<IExpression>();
+
+	expressions.Add(new TerminalExpression());
+	expressions.Add(new NonTerminalExpression());
+	expressions.Add(new TerminalExpression());
+
+	foreach(var expression in expressions)
+	{
+		expression.Interpret(context);
+	}
+}
---
+++ BehavioralPatterns/InterpreterPattern/Structural/NonTerminalExpression.cs
@@ -0,0 +1,9 @@
+﻿namespace InterpreterPattern.Structural;
+
+public class NonTerminalExpression : IExpression
+{
+	public void Interpret(Context context)
+	{
+		Console.WriteLine($"{GetType().Name}.{System.Reflection.MethodBase.GetCurrentMethod()?.Name}");
+	}
+}
